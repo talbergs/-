1a80a85d5773b74dd3d3951ce3b654b12eaa950c
Your Name <you@example.com>
2019-10-06 11:14:04 +0300

    automated commit Sun 06 Oct 2019 11:14:04 AM EEST

--- .config/fish/conf.d/fzf_key_bindings.fish
+++
@@ -1,6 +0,0 @@
-# @dotfile
-bind \co '__fzf_find_file'
-bind \cv '__fzf_edit_file'
-bind \cr '__fzf_reverse_isearch'
-
-# TODO: bind <c-return> to rotate word ahead and sumbit prompt. Ie to see manpage for cronie: cronie man <c-return>
