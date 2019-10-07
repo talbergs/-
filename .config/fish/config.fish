7c69dee620664e1e2f3ab03965c104d261539fbf
Your Name <you@example.com>
2019-10-07 22:23:38 +0300

    automated commit Mon 07 Oct 2019 10:23:38 PM EEST

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,15 @@ set -gx BROWSER chromium
 set -gx XKB_DEFAULT_LAYOUT "us,lv(apostrophe)"
 set -gx XKB_DEFAULT_OPTIONS grp:alt_shift_toggle
 
+set -gx LESS_TERMCAP_mb \e'[1;31m'       #  begin bold
+set -gx LESS_TERMCAP_md \e'[1;33m'       #  begin blink
+set -gx LESS_TERMCAP_so \e'[01;44;37m'   #  begin reverse video
+set -gx LESS_TERMCAP_us \e'[01;37m'      #  begin underline
+set -gx LESS_TERMCAP_me \e'[0m'          #  reset bold/blink
+set -gx LESS_TERMCAP_se \e'[0m'          #  reset reverse video
+set -gx LESS_TERMCAP_ue \e'[0m'          #  reset underline
+set -gx MANPAGER        'less -s -M +Gg' #  verbose prompt + hlsearch
+
 # Global path preppend any bin folder in ~/.progs
 if test -d ~/.progs
   set -pgx PATH (du ~/.progs | cut -f2 | grep '/bin$')
@@ -26,7 +35,10 @@ if test -z (pgrep ssh-agent)
   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
   set -Ux SSH_AGENT_PID $SSH_AGENT_PID
   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-  # ssh-add ~/.ssh/id_rsa
+
+  if test -e ~/.ssh/id_rsa
+    ssh-add ~/.ssh/id_rsa
+  end
 end
 
 set fish_greeting # this unsets that greeting
---
+++ README.md
@@ -0,0 +1,6 @@
+To an instant rice from Arch installation media:
+```bash
+$ curl https://raw.githubusercontent.com/talbergs/-/master/.scripts/arch/arch-install > run.sh
+$ bash $_
+```
+
